f6e997cb30cb8508e9f5298006e0d84284bdaddf
Caio Vieira <contato@caiovieira.com.br>
2021-07-22 21:07:08 -0300

    refactor: home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect }  from 'react';
+
+import { ProductList } from './styles';
+import { api } from '../../services/api';
+import { formatPrice } from '../../util/format';
+
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  image: string;
+}
+
+interface ProductFormatted extends Product {
+  priceFormatted: string;
+}
 
 const Home = (): JSX.Element => {
 
+  const [products, setProducts] = useState<ProductFormatted[]>([]);
+
+  useEffect( () => {
+    async function loadProducts() {
+      const response = await api.get<Product[]>('product');
+      const data = response.data.map(product => ({
+        ...product,
+        priceFormatted: formatPrice(product.price)
+      }));
+      setProducts(data);
+    }
+    loadProducts();
+  }, []);
 
   return (
-    <h1>Teste</h1>
+    <ProductList>
+      {products.map(product => (
+        <li key={product.id}>
+          <img src={product.image} alt={product.name} />
+          <strong>{product.name}</strong>
+          <span>{product.priceFormatted}</span>
+          <button
+            type="button"
+          onClick={() => console.log('cliquei')}
+          >
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+      ))}
+    </ProductList>
   );
 };
 
